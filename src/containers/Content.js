1b5a04b660e18112eeefd1594a15d33ac56f6c91
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-01-17 20:29:39 -0800

    Added posts and flex layout

--- src/containers/Content.js
+++ src/containers/Content.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Post from './Post'
 
 const Content = () => {
     return (
         <div className="content">
             <h2>Content:</h2>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
         </div>
     )
 }
---
+++ src/containers/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div className="post">
+            <h3>I'm a post</h3>
+        </div>
+    )
+}
+
+export default Post
