c29311f1b61d99dd56f69e371b0bbcfe820dd546
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-01-19 00:50:33 -0800

    Added like capability. Small style changes. Links open in new tab

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 
-const Post = ({"data": {img_src, rover, camera, earth_date, sol}}) => {
+const Post = ({"data": {img_src, rover, camera, earth_date}}) => {
+
+    const [liked, setLiked] = useState(false)
+
+    const handleLike = liked => {
+        setLiked(!liked)
+    }
+
+    const renderLikeChar = () => {
+        if(liked){
+            return String.fromCharCode("0x2764")
+        } else {
+            return String.fromCharCode("0x2661")
+        }
+    }
 
 
-    // debugger
     return (
         <div className="post">
-            <a href={img_src}>
+            <a target="_blank" rel="noopener noreferrer" href={img_src}>
             <img src={img_src}></img>
             </a>
             <h3>{rover["name"]} - {camera["full_name"]}</h3>
             <p>{earth_date}</p>
-            <button>{String.fromCharCode(9829)}</button>
+            <button onClick={() => handleLike(liked)}>{renderLikeChar()}</button>
         </div>
     )
 }
